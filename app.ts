23c043355e4600b7896566d68d3ac1299868bf03
doodi pincus <d0533114605@gmail.com>
2023-08-28 17:13:07 +0300

    including cors

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productsRouter from './api/product/products.router';
 import usersRouter from './api/users/users.router';
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
